18caca3a5cea60f581460bd4b1501225fbd5844f
Manuel1426 <immaosas@gmail.com>
2024-04-20 09:29:26 -0700

    Web framework

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,40 +1,43 @@
 #!/usr/bin/python3
-""" Script that runs a Flask app """
-from flask import Flask, escape
+""" Script that starts a Flask web application """
+from flask import Flask
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def hello():
-    """ function that returns on url /"""
+def hello_hbnb():
+    """ Print Web """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def hbnb():
-    """ function that returns on url /hbnb"""
+    """ Print Web """
     return 'HBNB'
 
 
 @app.route('/c/<text>')
-def hello_c(text):
-    """ function that returns a input"""
-    return 'C %s' % escape(text.replace('_', ' '))
+def c_is_fun(text):
+    """ Print a char C followed by the value of the text variable """
+    return 'C {}'.format(text.replace('_', ' '))
 
 
-@app.route("/python/", defaults={"text": "is cool"})
+@app.route('/python')
 @app.route('/python/<text>')
-def hello_python(text):
-    """ function that returns a input and has a default"""
-    return 'Python %s' % escape(text.replace('_', ' '))
+def python_is_cool(text='is cool'):
+    """ Print Python, followed by the value of the text variable,
+    with default value of text: is cool """
+    return 'Python {}'.format(text.replace('_', ' '))
 
 
-@app.route("/number/<int:n>")
+@app.route('/number/<int:n>')
 def number(n):
-    """ function that returns a number"""
-    return '%d is a number' % n
+    """ number route """
+    return '{:d} is a number'.format(n)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    app.run(debug=True)
     app.run(host='0.0.0.0', port=5000)
